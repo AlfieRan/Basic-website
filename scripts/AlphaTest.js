ddc1b0d14430d7349937407e2fdbc9c3775c5392
AlfieRan <89932058+AlfieRan@users.noreply.github.com>
2021-09-24 15:47:23 +0100

    added questions

--- scripts/AlphaTest.js
+++ scripts/AlphaTest.js
@@ -1,10 +1,46 @@
+
+let formDocument = document.forms['isUserAlpha'];
+let FailedAlpha = document.getElementById("FailedTestThing");
+
 console.log("it's alpha time");
+formDocument.hidden = false;
+FailedAlpha.hidden = true;
 
 function FailedTest(){
     console.log("Failed");
+    formDocument.hidden = true;
+    FailedAlpha.hidden = false;
 }
 
 function CalcResults(){
-    let playerAnswerRaw = document.forms['isUserAlpha'];
-    console.log(playerAnswerRaw);
+    var Score = 0;
+    if (formDocument["Q1"].value === "alpha")
+    {
+        Score += 1;
+    }
+    if (formDocument["Q2"].value === "apples")
+    {
+        Score += 1;
+    }
+    if (formDocument["Q3"].value === "yes")
+    {
+        Score += 1;
+    }
+    if (formDocument["Q4"].value === "scone")
+    {
+        Score += 1;
+    }
+    if (formDocument["Q5"].value === "yes")
+    {
+        Score += 1;
+    }
+
+
+    console.log(Score);
+}
+
+function RetryTest(){
+    console.log("Trying Again");
+    formDocument.hidden = false;
+    FailedAlpha.hidden = true;
 }
