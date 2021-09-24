a5ba4e055858cecf9555f00279e54dd40a7effe9
AlfieRan <89932058+AlfieRan@users.noreply.github.com>
2021-09-24 10:45:57 +0100

    started form

--- scripts/AlphaTest.js
+++ scripts/AlphaTest.js
@@ -1,1 +1,10 @@
 console.log("it's alpha time");
+
+function FailedTest(){
+    console.log("Failed");
+}
+
+function CalcResults(){
+    let playerAnswerRaw = document.forms['isUserAlpha'];
+    console.log(playerAnswerRaw);
+}
